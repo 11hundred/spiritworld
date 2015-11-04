746091d42d11e1fa5cf6e68c7d688639706852c1
Robert Sefer <rsefer@gmail.com>
2015-11-04 15:33:03 -0600

    Basic AJAX editing for post events

--- app/controllers/post_events_controller.rb
+++ app/controllers/post_events_controller.rb
@@ -2,7 +2,9 @@ class PostEventsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:new, :edit]
   before_action :set_post_events_count, only: [:new, :edit]
-	before_action :set_post_event, except: [:new, :create]
+  before_action :all_post_events, only: [:create, :update, :destroy]
+  before_action :set_post_event, only: [:edit, :update, :destroy]
+  respond_to :html, :js
 
   def new
     @post_event = PostEvent.new
@@ -15,40 +17,29 @@ class PostEventsController < ApplicationController
     @post_event.time_start = convert_mhs_to_seconds(params[:post_event][:time_start])
     @post_event.time_end = convert_mhs_to_seconds(params[:post_event][:time_end])
     @post_event.save
-    redirect_to edit_post_path(@post)
-  end
-
-  def edit
-
   end
 
   def update
-    respond_to do |format|
-      if @post_event.update(post_event_params)
-        @post_event.time_start = convert_mhs_to_seconds(params[:post_event][:time_start])
-        @post_event.time_end = convert_mhs_to_seconds(params[:post_event][:time_end])
-        @post_event.save
-        format.html { redirect_to edit_post_path(@post_event.post), notice: 'Post Event was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
-    end
+    @post_event.update(post_event_params)
+    @post_event.time_start = convert_mhs_to_seconds(params[:post_event][:time_start])
+    @post_event.time_end = convert_mhs_to_seconds(params[:post_event][:time_end])
+    @post_event.save
   end
 
   def destroy
     @post_event.destroy
-    respond_to do |format|
-      format.html { redirect_to edit_post_path(@post_event.post) }
-    end
   end
 
   private
-  	def set_post_event
+    def all_post_events
+      @post_events = PostEvent.where(:post_id => params[:post_id])
+    end
+
+    def set_post_event
       @post_event = PostEvent.find(params[:id])
     end
 
     def set_post
-      logger.debug "POST ID: #{params[:post_id]}"
       @post = Post.find(params[:post_id])
     end
 
@@ -67,7 +58,6 @@ class PostEventsController < ApplicationController
 
     def convert_mhs_to_seconds(given_mhs)
       given_blocks_reversed = given_mhs.scan(/([0-9]{2})+/).reverse
-      logger.debug given_blocks_reversed
       converted_seconds = 0
       given_blocks_reversed.each.with_index do |gbr, index|
         converted_seconds += gbr[0].to_i * (60 ** index)
